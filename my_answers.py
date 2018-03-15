3597920246b774314b0a0ebb29127dec43c06743
Rohit Patel <rpatel124@nam.ad.pwcinternal.com>
2018-03-15 00:51:28 -0500

    Update to the RNN part 1

--- my_answers.py
+++ my_answers.py
@@ -11,14 +11,14 @@ def window_transform_series(series, window_size):
 
 def build_part1_RNN(window_size):
     mod1 = Sequential()
-    mod1.add(LSTM(units = 5, input_shape =(window_size,1),return_sequences=True ))
-    mod1.add(LSTM(units = 1,return_sequences=False))
+    mod1.add(LSTM(units = 5, input_shape =(window_size,1)))
+    mod1.add(Dense(1))
     return mod1
 
 def cleaned_text(text):
     # punctuation = ['!', ',', '.', ':', ';', '?']
     import re
-    return re.sub(' +', ' ', re.sub('(?![a-z!,.:;?]).+?',' ',text))
+    return re.sub(' +', ' ', re.sub('(?![a-z!,.:;?]).+?',' ',text.replace('\n',' ').replace('\r',' ')))
 
 def window_transform_text(text, window_size, step_size):
     inputs, outputs = zip(*[(text[i:i+window_size],text[i+window_size] ) for i in range(0,len(text)-window_size,step_size)])
@@ -26,7 +26,7 @@ def window_transform_text(text, window_size, step_size):
 
 def build_part2_RNN(window_size, num_chars):
     mod1 = Sequential()
-    mod1.add(LSTM(units = 200, input_shape =(window_size,num_chars),return_sequences=True ))
+    mod1.add(LSTM(units = 21, input_shape =(window_size,num_chars),return_sequences=True ))
     mod1.add(LSTM(units = num_chars,return_sequences=False))
-    mod1.add(Activation('softmax'))
+    mod1.add(Dense(1, activation='softmax'))
     return mod1
